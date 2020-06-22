e782409a438bb0da188c63f505dc6312ea665cbe
Eugene Fedorov <Evgeniy.Fedorov@appline.ru>
2020-06-22 19:03:11 +0300

    Fix 4.1;

--- src/main/java/ru/moodle/Appline/course/FourthQuestion/RandomArray.java
+++ src/main/java/ru/moodle/Appline/course/FourthQuestion/RandomArray.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class RandomArray {
     public static void main(String[] args) {
         int[] arrayInt = new int [20];
-        int minElement, maxElement, buffer;
+        int minElement, maxElement, buffer, iter = 0;
         Random rand = new Random();
         /*Randomly filling the array*/
         for (int i = 0; i < 20; i++){
@@ -16,12 +16,24 @@ public class RandomArray {
         for (int i = 0; i < 20; i++) {
             System.out.print(arrayInt[i]+" ");
         }
-        maxElement = minElement = arrayInt[0];
+        maxElement = minElement = 0;
+        while ( iter < 20){
+            if (arrayInt[iter]>0){
+                minElement = iter;
+            } if (arrayInt[iter]<0){
+                maxElement = iter;
+            } if (maxElement != 0 & minElement != 0){
+                iter = 20;
+            } else {
+                iter++;
+            }
+        }
+
         /*Find min and max elements*/
         for (int i = 0; i < 20; i++) {
             if ((arrayInt[i] > 0) & (arrayInt[i] < arrayInt[minElement])){
                 minElement = i;
-            } if ((arrayInt[i] < 0) & (arrayInt[i] < arrayInt[maxElement])){
+            } if ((arrayInt[i] < 0) & (arrayInt[i] > arrayInt[maxElement])){
                 maxElement = i;
             }
         }
